40c35be2e2649b3d8730d53e5400a5f730acacdf
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-09-04 20:55:25 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 
 import Navbar from "./components/Navbar";
-
+import Main from "./components/Main";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
-
+import CursosDetalle from "./components/CursosDetalle";
+import cursosData from "./DATA/cursosData.json"; // Importa los datos desde el archivo JSON
 import Registerform from "./components/Registerform";
 
 import ChoseUserName from "./rutas/ChoseUserName";
@@ -14,10 +15,13 @@ function App() {
       <Navbar />
 
       <Routes>
-
+        <Route path="/MUNDO-INFORMATICA" element={<Main />} />
         <Route path="/choose-username" element={<ChoseUserName />} />
 
-        <Route path="/curso/:id" />
+        <Route
+          path="/curso/:id"
+          element={<CursosDetalle cursosData={cursosData} />}
+        />
         <Route path="/login" element={<Registerform />} />
       </Routes>
 
