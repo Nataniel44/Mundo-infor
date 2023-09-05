d5765bf0a0c78993afd5a1aab16c27bc187b3d7d
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-09-04 21:01:59 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/Mundo-infor/",
-  esbuild: {
-    jsxFactory: "React.createElement",
-    jsxFragment: "React.Fragment",
-  },
 });
